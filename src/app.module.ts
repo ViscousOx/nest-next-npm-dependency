ec40dda75f9d73fec4c64e2b23cdee02bcb17bd0
ViscousOx <jeffreylstamp@gmail.com>
2022-09-14 12:04:05 -0500

    connect nest and next

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { RenderModule } from 'nest-next';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import Next from 'next';
 
 @Module({
-  imports: [],
+  imports: [
+    RenderModule.forRootAsync(
+      Next({ dev: true }),
+      /* null means that nest-next 
+        should look for pages in root dir */
+      { viewsDir: null },
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
