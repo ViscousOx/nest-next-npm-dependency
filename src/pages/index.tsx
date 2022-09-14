e004c62d78e1eb2f1ea8f3bef108cdbc752c4125
ViscousOx <jeffreylstamp@gmail.com>
2022-09-14 14:12:59 -0500

    fix next build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <h1>Hello from NextJS! - Home</h1>;
+  return <h1>Hello from NextJS! - Hoe</h1>;
 };
 
 export default Home;
